72df089057b23fa205040b7830e5ff018e387e7a
Hung Quach <quachio@icloud.com>
2019-12-18 20:24:18 -0800

    added server.py

---
+++ server.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template, redirect, session, request, flash, get_flashed_messages
+
+app = Flask(__name__)
+app.secret_key = '7VL9f(u]ksirecbW4YMfBmAt37Tb@7MfEk;)i(eeaJMXqi2M2,nD+ya+aD&6gz3P'
+
+@app.route('index.html')
+def index():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
